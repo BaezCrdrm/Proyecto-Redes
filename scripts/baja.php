291d268eb7f86f806177b623c39256511ec662a1
BaezCrdrm <dev.baez_crdrm@outlook.com>
2016-05-25 10:30:42 -0500

    Del

--- scripts/baja.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function bajaPersona($id)
-    {
-        $query = "DELETE FROM usuarios WHERE id='".$id."'";
-        require "consultas.php";
-        $myBool = genQuery($query);
-        return $myBool;
-    }
-?>
